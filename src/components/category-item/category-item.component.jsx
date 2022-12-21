776fd627b6a7f9ce243a771c6595a4f2674e927e
Tomek Rychtyk <tomek@rychtyk.pl>
2022-12-21 19:02:59 +0100

    First commit

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,22 @@
+import './category-item.styles.scss';
+
+const CategoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+  return (
+    <div className='category-container'>
+      <div
+        className='background-image'
+        style={{
+          background: `url(${imageUrl})`,
+          border: '1px solid red',
+        }}
+      />
+      <div className='category-body-container'>
+        <h2>{title}</h2>
+        <p>Show now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
