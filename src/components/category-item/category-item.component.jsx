0beb855645a4cd43efdf4928ba76609498dcd922
Tomek Rychtyk <tomek@rychtyk.pl>
2022-12-22 19:11:34 +0100

    Added firebase auth

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -13,7 +13,7 @@ const CategoryItem = ({ category }) => {
       />
       <div className='category-body-container'>
         <h2>{title}</h2>
-        <p>Show now</p>
+        <p>SHOP NOW</p>
       </div>
     </div>
   );
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,53 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyD97iDntcLMql9L5aKuZIjFvutYFNV0iZM',
+  authDomain: 'tr-react-shop.firebaseapp.com',
+  projectId: 'tr-react-shop',
+  storageBucket: 'tr-react-shop.appspot.com',
+  messagingSenderId: '91040625227',
+  appId: '1:91040625227:web:d2d9d3ffef7013e0e413ce',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
